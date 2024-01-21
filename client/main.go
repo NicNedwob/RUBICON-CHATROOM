551ec40f18a513028edd17e99f2f849f8c0b9647
Nicholas Bowden <71820500+NicNedwob@users.noreply.github.com>
2024-01-21 17:17:48 +0200

    restructing

--- client/main.go
+++ client/main.go
@@ -2,60 +2,20 @@ package main
 
 import (
 	"bufio"
+	"client/clientutils"
 	"fmt"
 	"net"
 	"os"
-	"sync"
 )
 
-// Structs
-type ServerMsg struct {
-	server_msg string
-	mu         sync.Mutex
-}
-
 // Global Variables
-var srv_msg_struct = ServerMsg{server_msg: ""}
-
-func SendMessageToServer(msg string, conn net.Conn) {
-	switch msg {
-	case "Bye":
-		msg = msg + "\n"
-		conn.Write([]byte(msg))
-		conn.Close()
-		os.Exit(0)
-	case "":
-		return
-	default:
-		msg = msg + "\n"
-
-		conn.Write([]byte(msg))
-	}
-}
-
-func (srv_msg *ServerMsg) ReceiveMessageFromServer(conn net.Conn) {
-	for {
-		srv_msg.mu.Lock()
-
-		msg, err := bufio.NewReader(conn).ReadString('\n')
-		if err != nil {
-			fmt.Println("Server no longer available.")
-			fmt.Println("Exiting Application.")
-			os.Exit(1)
-		}
-		srv_msg.server_msg = msg
-		fmt.Print(srv_msg.server_msg)
-		srv_msg.mu.Unlock()
-
-	}
-
-}
+var srv_msg_struct = clientutils.ServerMsg{server_msg: ""}
 
 func main() {
 	fmt.Println("Starting Rubicon Chatroom Client")
 	scanner := bufio.NewScanner(os.Stdin)
 	conn, err := net.Dial("tcp", "localhost:8080")
-	defer SendMessageToServer("Bye", conn)
+	defer clientutils.SendMessageToServer("Bye", conn)
 
 	if err != nil {
 		// handle error
@@ -68,12 +28,12 @@ func main() {
 	if srv_msg_struct.server_msg == "Please enter a username:\n" {
 		for scanner.Scan() {
 			msg := scanner.Text()
-			SendMessageToServer(msg, conn)
+			clientutils.SendMessageToServer(msg, conn)
 		}
 	}
 
 	for scanner.Scan() {
 		msg := scanner.Text() // Get the current line of text
-		SendMessageToServer(msg, conn)
+		clientutils.SendMessageToServer(msg, conn)
 	}
 }
