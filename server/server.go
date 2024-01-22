b50dcb65ee5b1556a4c48f5dcef0f9eab9296c5a
NicNedwob <nicholasbowden0@gmail.com>
2024-01-22 09:34:33 +0200

    restructuring for docker

---
+++ server/server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/NicNedwob/RUBICON-CHATROOM/server/internal/server"
+)
+
+// Global Variables
+
+func main() {
+	fmt.Println("Starting Rubicon Chatroom Server")
+	ln, err := net.Listen("tcp", "localhost:8080") // localhost
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		// create new go routine for each client
+		go server.HandleConnection(conn)
+	}
+}
